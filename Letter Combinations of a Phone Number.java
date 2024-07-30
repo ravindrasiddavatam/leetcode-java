d7ea95be27a9ab05ebd02ada00483940d93ed8fc
Ravindra <149860695+ravindrasiddavatam@users.noreply.github.com>
2024-07-30 00:19:48 -0500

    Letter Combinations of a Phone Number

---
+++ Letter Combinations of a Phone Number.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    private static final String[] KEYPAD = {
+        "",
+        "",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz"
+    };
+
+    public List<String> letterCombinations(String digits) {
+        
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.isEmpty()) {
+            return result;
+        }
+        backtrack(result, digits, new StringBuilder(), 0);
+        return result;
+
+    }
+
+    private void backtrack(List<String> result, String digits, StringBuilder current, int index){
+        if(index == digits.length()){
+            result.add(current.toString());
+            return;
+        }
+
+        String letters = KEYPAD[digits.charAt(index) - '0'];
+        for(char c: letters.toCharArray()) {
+            current.append(c);
+            backtrack(result, digits, current, index + 1);
+            current.deleteCharAt(current.length() - 1);
+
+        }
+    }
+
+
+}
