269858cc5a5bb2c68b5ab9406ceb86f7b26f9292
Ravindra <149860695+ravindrasiddavatam@users.noreply.github.com>
2024-08-04 15:34:16 -0500

    Maximum Subarray

---
+++ Maximum Subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int currentMax = nums[0];
+        int globalMax = nums[0];
+
+        for(int i=1; i<nums.length; i++) {
+            currentMax = Math.max(nums[i], currentMax + nums[i]);
+
+            if(currentMax > globalMax) {
+                globalMax = currentMax;
+            }
+        }
+            return globalMax;
+    }
+}
