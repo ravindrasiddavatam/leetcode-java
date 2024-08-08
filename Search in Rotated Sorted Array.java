2de348a73f0b8565a7f2dbbc1d6e312193a3e2c8
Ravindra <149860695+ravindrasiddavatam@users.noreply.github.com>
2024-08-08 01:18:33 -0500

    Search in Rotated Sorted Array

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,52 @@
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            // Determine which part is sorted
+            if (nums[left] <= nums[mid]) {
+                // Left part is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // Right part is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Example 1
+        int[] nums1 = {4, 5, 6, 7, 0, 1, 2};
+        int target1 = 0;
+        System.out.println(solution.search(nums1, target1)); // Output: 4
+
+        // Example 2
+        int[] nums2 = {4, 5, 6, 7, 0, 1, 2};
+        int target2 = 3;
+        System.out.println(solution.search(nums2, target2)); // Output: -1
+
+        // Example 3
+        int[] nums3 = {1};
+        int target3 = 0;
+        System.out.println(solution.search(nums3, target3)); // Output: -1
+    }
+}
