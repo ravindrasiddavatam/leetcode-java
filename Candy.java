5cc626571791214d6e76b917037787bb5e493350
Ravindra <149860695+ravindrasiddavatam@users.noreply.github.com>
2024-08-23 12:18:34 -0500

    Candy.java

---
+++ Candy.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        
+        // Step 1: Initialize all candies with 1
+        for (int i = 0; i < n; i++) {
+            candies[i] = 1;
+        }
+        
+        // Step 2: Left to right pass
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        
+        // Step 3: Right to left pass
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        
+        // Step 4: Calculate the total number of candies
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+        
+        return totalCandies;
+    }
+}
